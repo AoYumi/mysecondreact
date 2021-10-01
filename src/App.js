07972e0964e5097f5e257c0bdf45a866bc976f29
AoYumi <yubluemi@gmail.com>
2021-10-01 18:29:38 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://maikerucode.github.io/myfirstreact"
+          href="https://cyapher.github.io/myfirstreact/"
           target="_blank"
           rel="noopener noreferrer"
         >
