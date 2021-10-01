4f2ae9d8bf25ce51c7fb832c22cd9144b5ba4e27
AoYumi <yubluemi@gmail.com>
2021-10-01 16:26:31 +0800

    Yaes

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://imgur.com/OyXhPM6.png" className="App-logo" alt="logo" />
+        <p>
+          いいや~~
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Yamete Kudasai nii-sama~~
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
